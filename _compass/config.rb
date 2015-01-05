1d1f6a1c3f78fc1cbe175b9e69ce84289527e5a5
Harris Lapiroff <harris@chromamine.com>
2015-01-04 22:48:51 -0800

    Added ruby dependencies and new compass-based styles.

---
+++ _compass/config.rb
@@ -0,0 +1,13 @@
+require 'compass/import-once/activate'
+require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "../static/stylesheets"
+sass_dir = "sass"
+images_dir = "../static/images"
+fonts_dir = "../static/fonts"
+javascripts_dir = "../static/javascripts"
+output_style = :compressed
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
