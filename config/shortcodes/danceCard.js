28865f4a4b97aa704ccd0a3c0e963b71064e01db
Harris Lapiroff <harris@freedom.press>
2024-05-18 12:34:17 -0400

    Add nantucket contra dance blog post
    Also adds a bunch of plugin code and styles to support new contra
    dance card features

---
+++ config/shortcodes/danceCard.js
@@ -0,0 +1,27 @@
+const danceCard = (danceCollection, title) => {
+    const dance =  danceCollection.find(d => d.title === title)
+    const sections = Object.entries(dance.choreo)
+    const output = `<div class="dance-card">
+        <header class="dance-header">
+            <div class="dance-title">${dance.title}</div>
+            <div class="dance-author">${dance.author}</div>
+            <div class="dance-formation">${dance.formation}</div>
+        </header>
+        <div class="dance-choreo">
+            ${sections.map(([k, v]) => `
+                <div class="dance-section">
+                    <div class="dance-section-label">
+                        ${k.toUpperCase()}
+                    </div>
+                    <div class="dance-section-lines">
+                        ${v.map(l => `${l}`).join('<br />')}
+                    </div>
+                </div>
+            `).join('')}
+        </div>
+    </div>`.replace(/([\n\r]|[\s]{4})/g, '')
+    console.log(output)
+    return output
+}
+
+module.exports = danceCard
--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -3,4 +3,5 @@ const imageGrid = require('./imageGrid')
 module.exports = [
     ['paired', 'imagegrid', imageGrid],
     ['single', 'stl', require('./stl')],
+    ['single', 'dancecard', require('./danceCard')],
 ]
