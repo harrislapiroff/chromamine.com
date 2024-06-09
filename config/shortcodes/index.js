181daffeb4054c3e56c8713349a8dcd2700e124a
Harris Lapiroff <harris@freedom.press>
2024-06-09 17:47:07 -0400

    Add new blog post about Spotify to Apple Music shortcut

--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -1,7 +1,9 @@
 const imageGrid = require('./imageGrid')
+const button = require('./button')
 
 module.exports = [
     ['paired', 'imagegrid', imageGrid],
     ['single', 'stl', require('./stl')],
     ['single', 'dancecard', require('./danceCard')],
+    ['single', 'button', button],
 ]
