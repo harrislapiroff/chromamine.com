73144b48691b1487f3cd3665f481db8058f2059f
Harris Lapiroff <harris@freedom.press>
2024-07-17 23:17:51 -0400

    Use rollup to compile Observable client-side scripts
    Non-functional--haven't figured out how to get imports to resolve correctly

--- config/utils/omd/compile.mjs
+++ config/utils/omd/compile.mjs
@@ -2,8 +2,9 @@ import markdownIt from 'markdown-it'
 
 import { createMarkdownIt } from '@observablehq/framework/dist/markdown.js'
 import { transpileJavaScript } from '@observablehq/framework/dist/javascript/transpile.js'
-import { getResolvers } from '@observablehq/framework/dist/resolvers.js'
 import { rewriteHtml } from '@observablehq/framework/dist/html.js'
+
+import { getResolvers } from './resolvers.mjs'
 
 // Loosely based on parseMarkdown from Observable Framework
 // https://github.com/observablehq/framework/blob/05d70c58dc4639c586c5669d30901eadbe102213/src/markdown.ts#L238
@@ -15,21 +16,35 @@ async function parseOmd(omdSource, options) {
     // Running this function will mutate `code` which we can use to generate
     // the live javascript portion of the code
     const body =  md.renderer.render(tokens, md.options, context)
-    return { body, code }
+    return {
+        body, // The HTML contents of the body
+        code, // The javascript code blocks with metadata to be rendered into a script
+
+        // These are the remaining properties that Observable's parseMarkdown returns.
+        // For the most part we don't need them because the function they serve in OF
+        // doesn't apply when using it just as a template language. But we leave them
+        // here as null in case any OF functions expect them.
+        title: null,
+        head: null,
+        data: {},
+        header: null,
+        footer: null,
+        style: null,
+    }
 }
 
 // Bits and bobs borrowed from renderPage from Observable Framework
 // https://github.com/observablehq/framework/blob/05d70c58dc4639c586c5669d30901eadbe102213/src/render.ts#L27
-async function generateClientScript(page, clientDir, resolvers) {
+async function generateClientScript(page, clientPath, resolvers) {
     return `
-        import { define } from '${clientDir}main.js'
+        import { define } from '${clientPath}main.js'
 
         ${page.code.map(
             ({node, id, mode}) => transpileJavaScript(
                 node,
                 {
                     id,
-                    path: clientDir,
+                    path: clientPath,
                     mode,
                     resolveImport: resolvers.resolveImport
                 }
@@ -38,18 +53,16 @@ async function generateClientScript(page, clientDir, resolvers) {
     `
 }
 
-export async function compileObservableMD(omdSource, options) {
-    const { clientDir } = options
+export async function compileObservableMD(omdSource, data, options) {
+    const { clientPath, clientDir } = options
 
-    const page = {...await parseOmd(omdSource), data: {}}
+    const page = await parseOmd(omdSource)
 
-    const resolvers = await getResolvers(page, {
-        root: '',
-    })
+    const resolvers = await getResolvers(page, { root: clientDir })
 
     const clientScript = await generateClientScript(
         page,
-        clientDir,
+        clientPath,
         resolvers,
     )
     return `
