f2950ac4c04c93b75d0da8c8e14b3e38b44bb454
Harris Bard Lapiroff <harris@chromamine.com>
2024-12-25 21:56:13 -0800

    Add conditional that allows modules that aren't lazy-loaded to work

--- config/utils/ojs/client/global.js
+++ config/utils/ojs/client/global.js
@@ -1,8 +1,13 @@
 export default async function(name) {
     const stdlib = await d3.require('https://www.unpkg.com/@observablehq/stdlib@5.8.8/dist/stdlib.js')
     const library = new stdlib.Library()
+    console.log(library)
     try {
-        return library[name]()
+        // If it's a function, we call it to load the module
+        if (typeof library[name] === 'function') return library[name]()
+        // Otherwise we return it as is
+        // See: https://github.com/observablehq/stdlib/blob/main/src/library.js#L74-L76
+        return library[name]
     } catch(e) {
         console.log(`Failed to load from the Observable stdlib: ${name}`)
     }
